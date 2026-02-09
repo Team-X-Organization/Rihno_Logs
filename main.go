210cef50a9851ddb7fa92354846c10836d8d1d57
Sakib_Dalal <sakibdalal73@gmail.com>
2026-02-09 18:14:46 +0530

    Initial Commit

---
+++ main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/shirou/gopsutil/v3/cpu"
+	"github.com/shirou/gopsutil/v3/host"
+)
+
+func main() {
+	// cpu percentage
+	cpuPercentAvg, err := cpu.Percent(0, false)
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Println(cpuPercentAvg)
+
+	cpuPercentMulti, err := cpu.Percent(0, true)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	fmt.Println(cpuPercentMulti)
+
+	// cpu temperature
+	// SensorsTemperatures returns a slice of all detected sensors
+	// (CPU, GPU, Battery, etc. depending on the OS)
+	sensorStats, err := host.SensorsTemperatures()
+	if err != nil {
+		log.Fatalf("Failed to get sensor data: %v", err)
+	}
+
+	if len(sensorStats) == 0 {
+		fmt.Println("No temperature sensors detected.")
+		return
+	}
+
+	fmt.Println("Detected Temperatures:")
+	for _, sensor := range sensorStats {
+		fmt.Printf("Sensor: %-20s | Temp: %.2f°C\n", sensor.SensorKey, sensor.Temperature)
+	}
+}
